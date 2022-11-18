1cdb40b85f286c7b07a57f7fcd3665a30733ef90
mohaiminul375 <mohaiminul375@gmail.com>
2022-11-18 22:09:49 +0600

    server is working

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/',async(req,res)=>{
+    res.send('doctors portal server is running')
+})
+
+app.listen(port,()=>console.log(`doctors portal server is running ${port}`))
