95c5d1fb701a9b9ce3ca9c9c9eb7075263a041d5
mohaiminul375 <mohaiminul375@gmail.com>
2022-11-19 18:48:18 +0600

    post booking working

--- index.js
+++ index.js
@@ -1,59 +1,49 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { query } = require('express');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { query } = require("express");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixszr3u.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-try{
-const appointmentOptionCollection= client.db('doctorsportal').collection('appointmentOptions')
+async function run() {
+  try {
+    const appointmentOptionCollection = client
+      .db("doctorsportal")
+      .collection("appointmentOptions");
+    const bookingCollection = client.db("doctorsportal").collection("booking");
 
-app.get('/appointmentOptions',async(req,res)=>{
-    const query = {};
-    const options = await appointmentOptionCollection.find(query).toArray();
-    res.send(options);  
-})
+    app.get("/appointmentOptions", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
 
-}
-finally{
 
-}
+    app.post('/bookings', async(req,res)=>{
+        const booking = req.body
+        const result = await bookingCollection.insertOne(booking)
+        res.send(result);
+    })
+  } finally {
+  }
 }
 
 run().catch(console.log);
 
+app.get("/", async (req, res) => {
+  res.send("doctors portal server is running");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-app.get('/',async(req,res)=>{
-    res.send('doctors portal server is running')
-})
-
-app.listen(port,()=>console.log(`doctors portal server is running ${port}`))
-
-
-
+app.listen(port, () => console.log(`doctors portal server is running ${port}`));
