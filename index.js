494c3783fe47e360224435e7c9039650eabb42f5
mohaiminul375 <mohaiminul375@gmail.com>
2022-11-21 10:27:24 +0600

    complete 75

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
 
+//middleware
 app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { query } = require("express");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixszr3u.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -25,23 +26,47 @@ async function run() {
     const bookingCollection = client.db("doctorsportal").collection("booking");
 
     app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
+      const bookingQuery= {appointmentDate: date}
+      const alreadyBooked = await bookingCollection.find(bookingQuery).toArray();
+      options.forEach(option => {
+        const optionBooked = alreadyBooked.filter(book =>book.treatment === option.name)
+        const bookedSlots = optionBooked.map(book => book.slot)
+        const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+        option.slots = remainingSlots;
+      })
       res.send(options);
     });
 
 
-    app.post('/bookings', async(req,res)=>{
-        const booking = req.body
-        const result = await bookingCollection.insertOne(booking)
-        res.send(result);
-    })
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const query ={
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment
+      }
+      const alreadyBooked = await bookingCollection.find(query).toArray();
+
+      if(alreadyBooked.length){
+        const message = `You already have a booking on ${booking.appointmentDate}`
+        return res.send({acknowledged: false, message})
+      }
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+      // console.log(result)
+    });
   } finally {
   }
+  
 }
-
 run().catch(console.log);
 
+
+//testing 
 app.get("/", async (req, res) => {
   res.send("doctors portal server is running");
 });
